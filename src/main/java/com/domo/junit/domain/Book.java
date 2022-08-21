0677786d9cacea11286a64bbaf93eb51a146ea59
kdomo <dongho5088@naver.com>
2022-08-21 22:28:19 +0900

    Book Entity 생성

--- src/main/java/com/domo/junit/domain/Book.java
+++ src/main/java/com/domo/junit/domain/Book.java
@@ -1,4 +1,32 @@
 package com.domo.junit.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50)
+    @NonNull
+    private String title;
+
+    @Column(length = 20)
+    @NonNull
+    private String author;
+
+    @Builder
+    public Book(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
 }
