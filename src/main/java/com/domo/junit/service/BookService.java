c9fd0ea4b0886f78a80290ec52ad3f0e13d81384
kdomo <dongho5088@naver.com>
2022-08-23 15:05:04 +0900

    BookService/책/책 목록보기 완료

--- src/main/java/com/domo/junit/service/BookService.java
+++ src/main/java/com/domo/junit/service/BookService.java
@@ -8,19 +8,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class BookService {
-    //1.책 등록
+
     private final BookRepository bookRepository;
-    //2.책 목록보기
+    //1.책 등록
     @Transactional(rollbackFor = RuntimeException.class)
     public BookResDto 책_등록(BookReqDto bookReqDto){
         Book bookPS = bookRepository.save(bookReqDto.toEntity());
         return new BookResDto().toResDto(bookPS);
     }
-    //3. 책 한건 보기
-
+    //2.책 목록보기
+    public List<BookResDto> 책_목록보기(){
+        return bookRepository.findAll().stream()
+                .map(new BookResDto()::toResDto).collect(Collectors.toList());
+    }
     //4.책 삭제
 
     //5.책 수정
