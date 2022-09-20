6bd98364dc8406867d4af49151bda99faa6af919
kdomo <dongho5088@naver.com>
2022-09-20 12:10:32 +0900

    Controller IntegrationTest saveBook()

--- src/test/java/com/domo/junit/controller/BookApiControllerTest.java
+++ src/test/java/com/domo/junit/controller/BookApiControllerTest.java
@@ -1,62 +1,62 @@
 package com.domo.junit.controller;
 
-import com.domo.junit.service.BookService;
+
 import com.domo.junit.controller.request.BookReqDto;
-import com.domo.junit.controller.response.BookResDto;
-import org.junit.jupiter.api.DisplayName;
+import com.domo.junit.service.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.assertj.core.api.Assertions.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-@WebMvcTest(BookApiController.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class BookApiControllerTest {
 
     @Autowired
-    MockMvc mvc;
+    private BookService bookService;
 
-    @MockBean
-    BookService bookService;
+    @Autowired
+    private TestRestTemplate rt;
 
-    @Test
-    @DisplayName("책 목록 가져오기")
-    void getList() throws Exception {
-        List<BookResDto> list = new ArrayList<>();
-        list.add(BookResDto.builder().title("testTitle").author("testAuthor").build());
+    private static ObjectMapper om;
+    private static HttpHeaders headers;
 
-        given(bookService.getBookList()).willReturn(list);
-
-        mvc.perform(get("/api"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("testTitle")));
+    @BeforeAll
+    static void init(){
+        om = new ObjectMapper();
+        headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
     }
 
     @Test
-    @DisplayName("책 ")
-    void addBook() throws Exception {
+    void saveBook() throws Exception {
+        //given
         BookReqDto bookReqDto = new BookReqDto();
-        bookReqDto.setTitle("책등록");
-        bookReqDto.setAuthor("책등록");
+        bookReqDto.setTitle("책 제목");
+        bookReqDto.setAuthor("책 작가");
+        String body = om.writeValueAsString(bookReqDto);
 
-//        given(bookService.책_등록(bookReqDto)).willReturn(BookResDto.builder()
-//                        .author("책등록")
-//                        .title("책등록")
-//                        .build());
+        //when
+        HttpEntity<String> request = new HttpEntity<>(body, headers);
+        ResponseEntity<String> response = rt.exchange("/api/v1/books", HttpMethod.POST, request, String.class);
 
-        mvc.perform(post("/api"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("책등록")));
+        //then
+        DocumentContext dc = JsonPath.parse(response.getBody());
+        String title = dc.read("$.body.title");
+        String author = dc.read("$.body.author");
+
+        assertThat(title).isEqualTo("책 제목");
+        assertThat(author).isEqualTo("책 작가");
+        System.out.println(response.getBody());
+
 
     }
+
 }
